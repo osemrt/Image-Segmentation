b13a2bab71df23fa953714801ff34dc15e7d2fb4
osemrt <osemrt@gmail.com>
2019-10-20 11:21:20 +0300

    Implemented k-means algorithm

---
+++ k-means.cpp
@@ -0,0 +1,38 @@
+#include "header_files/declarations.h"
+// free heap memory!!
+void k_means(Mat* image, int N) {
+
+	pxLocation* centroids = generateRandomLocation(image, N);
+	vector< vector < pxLocation* > > clusteredPixels(N);
+	pxLocation** imagePixels = createImageLocationMatrix(image);
+
+
+	int closestCentroidIndex;
+	bool flag = true;
+
+	while (flag) {
+		flag = true;
+		for (int i = 0; i < image->rows; i++) {
+			for (int j = 0; j < image->cols; j++) {
+
+				closestCentroidIndex = findClosestCentroid(image, centroids, &imagePixels[i][j], N);
+
+				if (imagePixels[i][j].currentClusterId != closestCentroidIndex) {
+					extractPixelFromPreviousCluster(&clusteredPixels, &imagePixels[i][j]);
+					includePixelToCluster(image, &clusteredPixels, centroids[closestCentroidIndex], &imagePixels[i][j]);
+					flag = false;
+				}
+			}
+
+		}
+
+		if (!flag) {
+			recalculateCenterForClusters(&clusteredPixels, centroids, N);
+		}
+
+	}
+
+
+
+
+}
